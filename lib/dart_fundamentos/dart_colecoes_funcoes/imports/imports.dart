4e685fead37ade2850393270e6d2eca76048bf6c
vitor.Brussolo.zerbato.Houseasy <vitor.zerbato@houseasy.net>
2021-10-02 16:25:07 -0300

    aprendendo dart

---
+++ lib/dart_fundamentos/dart_colecoes_funcoes/imports/imports.dart
@@ -0,0 +1,9 @@
+import 'package:academi_flutter/dart_fundamentos/dart_colecoes_funcoes/imports/soma.dart';
+
+void main() {
+  var somaDouble1 = somaDouble(1.5, 2);
+  var somaInteiro1 = somaInteiros(2, 1);
+
+  print(somaInteiro1);
+  print(somaDouble1);
+}
